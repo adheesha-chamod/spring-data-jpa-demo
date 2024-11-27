ae191826134c904996ac924a9ba739556b71a7d7
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-11-27 22:00:49 +0530

    Add student repository

---
+++ src/main/java/com/demo/springDataJpaDemo/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.demo.springDataJpaDemo.repository;
+
+import com.demo.springDataJpaDemo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+}
