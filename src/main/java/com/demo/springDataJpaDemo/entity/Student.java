c5a4925d566d7374b4daa36dd2ceed1eecf851eb
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-11-27 21:53:36 +0530

    Add student entity

---
+++ src/main/java/com/demo/springDataJpaDemo/entity/Student.java
@@ -0,0 +1,40 @@
+package com.demo.springDataJpaDemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column(
+            name = "email_address",
+            unique = true,
+            nullable = false
+    )
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
